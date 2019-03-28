a47dd2b7a9410641bcb9aa043674c62b0ef63182
tankren <9392749+tankren@users.noreply.github.com>
2019-03-28 14:04:04 +0800

    Delete gdddns_config.sh

--- gdddns/scripts/gdddns_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "`dbus get gdddns_enable`" == "1" ]; then
-    dbus delay gdddns_timer `dbus get gdddns_interval` /koolshare/scripts/gdddns_update.sh
-else
-    dbus remove __delay__gdddns_timer
-fi
