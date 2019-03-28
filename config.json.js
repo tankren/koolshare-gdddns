45f49e6b93be1a69c90b5c5fe6a521d416eb46b4
tankren <9392749+tankren@users.noreply.github.com>
2019-03-28 13:51:13 +0800

    Delete config.json.js

--- config.json.js
+++
@@ -1,9 +0,0 @@
-{
-    "build_date": "2017-05-03_19:15:58", 
-    "description": "godaddy ddns！", 
-    "home_url": "Module_gdddns.asp", 
-    "md5": "1a944c2d9d952160ea3908d3370df849", 
-    "module": "gdddns", 
-    "title": "gdddns", 
-    "version": "1.0.1"
-}
