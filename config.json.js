2d09c92a070ff4e68bd0665d9b9f2656856cfbd6
tankren <9392749+tankren@users.noreply.github.com>
2019-03-28 13:57:50 +0800

    Create config.json.js

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "CloudFlare ddns！", 
+    "home_url": "Module_cfddns.asp", 
+    "md5": "efbce46c442ba9c1337f24394a1aa5d9", 
+    "module": "cfddns", 
+    "title": "cfddns", 
+    "version": "1.0.0"
+}
